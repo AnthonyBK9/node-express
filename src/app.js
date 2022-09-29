cc757d951f975e77f39e48d916cdaf79492d651d
AnthonyBK9 <antonio.bermudez.ing@gmail.com>
2022-09-28 22:08:38 -0500

    agregando verbos de las peticiones

--- src/app.js
+++ src/app.js
@@ -4,19 +4,19 @@ const app = express();
 const port = 8000;
 
 app.get('/me', (req, res) => {
-    res.status(200).json({name: 'Antonio', age: 25, country: 'México'});
+    res.status(200).json({name: 'Antonio', age: 25, country: 'México', verb: req.method});
 });
 
 app.post('/metas', (req, res) => {
-    res.status(200).json([{ hobbie1: 'Salir a acampar'}, {hobbie2: 'jugar videojuegos'}, {hobbie3: 'Salir a correr'}]);
+    res.status(200).json([{ hobbie1: 'Salir a acampar'}, {hobbie2: 'jugar videojuegos'}, {hobbie3: 'Salir a correr'}, {verb: req.method}]);
 });
 
 app.patch('/metas', (req, res) => {
-    res.status(200).json([{metas1:'Dominar Javascript a nivel avanzado'},{metas2:'Obtener las habilidades de Full Stack'}]);
+    res.status(200).json([{metas1:'Dominar Javascript a nivel avanzado'},{metas2:'Obtener las habilidades de Full Stack'},{verb: req.method}]);
 });
 
 app.put('/business', (req, res) => {
-    res.status(200).json([{company1:'SW Sapien'}, {company2:'Treinta'}, {company3:'Globant'}])
+    res.status(200).json([{company1:'SW Sapien'}, {company2:'Treinta'}, {company3:'Globant'},{verb: req.method}])
 });
 
 app.listen(port, () => {
